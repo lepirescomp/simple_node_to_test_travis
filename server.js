fea1add0534fabc690f33598bc22ba4d60f47880
leticia <lepirescomp@gmail.com>
2021-05-29 15:14:43 -0300

    add favorite food

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ function sleep(ms) {
 }
 
 async function main() {
+
+	const favoriteFood = process.env.FAVORITE_FOOD;
+	console.log(`My favorite food is ${favoriteFood}`);
+
+
   while(true) {
     console.log('Deploying!');
     await sleep(5000);
